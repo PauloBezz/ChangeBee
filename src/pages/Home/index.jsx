cf77bc06b3475b6e3017d14b13e0d7c1bf4b8d54
Paulo Henrique <paulo.bezerra2057@gmail.com>
2024-06-28 13:05:05 -0300

    fix: use ContentSmall

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,18 +1,16 @@
 import { Header } from './../../components/Header';
 import { Main } from './../../components/Main';
 import { Footer } from './../../components/Footer';
-import { CardSmall } from './../../components/Cards/CardSmall';
 import { CardPrice } from './../../components/Cards/CardsPrice';
+import ContentSmall from '../../components/Cards';
+
 
 export function Home() {
     return (
         <>
             <Header />
             <Main titleWhite={'Sua'} titleYellow={'mudança'} tWhiteSecond={'é'} tWhiteThree={'nosso'} tYellowSecond={'Negócio!'} subtitle={'Desde 1991, estamos carregados de mudança e recomeço pelo Brasil. Conhecemos o peso da dedicação por força de vontade, siga conosco.'} />
-            <CardSmall />
-            <CardSmall />
-            <CardSmall />
-            <CardSmall />
+            <ContentSmall/>
             <CardPrice />
             <CardPrice />
             <CardPrice />
