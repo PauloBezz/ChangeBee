4dcf1db424fba12b0ecaac5edbaee4bc79f3b90b
Paulo Henrique <paulo.bezerra2057@gmail.com>
2024-07-01 23:59:42 -0300

    refactor: add Footer component

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,6 +1,29 @@
+import styles from './styles.module.css'
+import { Link } from "react-router-dom"
+import { Whats } from "./assets/svg/Whatsapp"
+import { Face } from "./assets/svg/Facebook"
+import { Insta } from "./assets/svg/Instagram"
+
 export function Footer() {
     return (
         <>
+            <footer className={styles.container}>
+                <article className={styles.links}>
+
+                    <a href="">Trabalhe conosco</a>
+                    <a href="">Sobre nós</a>
+                    <div className={styles.icons}>
+                        <Link><Whats /></Link>
+                        <Link><Face /></Link>
+                        <Link><Insta /></Link>
+                    </div>
+                </article>
+                <article className={styles.text}>
+                    <p>Copyright © 2002-2024 Cat Soul. Respeito a todos os direitos de uso</p>
+                    <p>Desejamos um bom recomeço e reecontros na sua nova vida!</p>
+                </article>
+            </footer>
+
         </>
     )
 }
