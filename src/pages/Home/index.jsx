3282a99694525d8066560e0724e93cb828309088
Paulo Henrique <paulo.bezerra2057@gmail.com>
2024-07-24 18:52:55 -0300

    feat: add Costumer in Home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,6 +2,7 @@ import { Header } from './../../components/Header';
 import { Main } from './../../components/Main';
 import { Footer } from './../../components/Footer';
 import ContentSmall from '../../components/Cards';
+import Costumer from '../../components/Costumer';
 
 
 export function Home() {
@@ -10,7 +11,7 @@ export function Home() {
             <Header />
             <Main titleWhite={'Sua'} titleYellow={'mudança'} tWhiteSecond={'é'} tWhiteThree={'nosso'} tYellowSecond={'Negócio!'} subtitle={'Desde 1991, estamos carregados de mudança e recomeço pelo Brasil. Conhecemos o peso da dedicação por força de vontade, siga conosco.'} />
             <ContentSmall/>
-
+            <Costumer/>
             <Footer />
         </>
     )
