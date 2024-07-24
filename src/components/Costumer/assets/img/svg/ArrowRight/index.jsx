293fd450430d4cd359f1bc1e2e5716d4f4bf7a7b
Paulo Henrique <paulo.bezerra2057@gmail.com>
2024-07-24 18:51:49 -0300

    feat: added Costumer component

---
+++ src/components/Costumer/assets/img/svg/ArrowRight/index.jsx
@@ -0,0 +1,27 @@
+
+
+export default function ArrowR() {
+    return (
+        <svg width="43"
+            height="63"
+            viewBox="0 0 43 63"
+            fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g filter="url(#filter0_d_38_38)">
+                <path d="M5.375 31.5H37.625M37.625 31.5L22.3958 9.1875M37.625 31.5L22.3958 53.8125" stroke="#F1F1F1" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+            </g>
+            <defs>
+                <filter id="filter0_d_38_38" x="-4" y="0" width="51" height="71" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                    <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                    <feOffset dy="4" />
+                    <feGaussianBlur stdDeviation="2" />
+                    <feComposite in2="hardAlpha" operator="out" />
+                    <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+                    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_38_38" />
+                    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_38_38" result="shape" />
+                </filter>
+            </defs>
+        </svg>
+
+    )
+}
