e328b04afeed4de94f4337e2a03c116dd6baa035
Paulo Henrique <paulo.bezerra2057@gmail.com>
2024-07-30 23:57:04 -0300

    feat: add value in props the CardPrice component

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -5,6 +5,7 @@ import { Shield } from './assets/images/svg/Shield'
 import { Speed } from "./assets/images/svg/Speed"
 import { Squad } from "./assets/images/svg/Squad"
 import Banner from "../Banner"
+import { CardPrice } from './CardPrice'
 
 export const SmallInfo = [
     {
@@ -26,6 +27,36 @@ export const SmallInfo = [
 
 ]
 
+export const PriceInfo = [
+    {
+        title: 'Pacote para transporte seco',
+        image: '0',
+        info: 'Equipe de apoio',
+        infoTwo: 'Segundo Motorista',
+        infoThree: 'Apenas R$9,99/Km',
+        infoFour: '',
+        price: '1599,90',
+    },
+    {
+        title: 'Pacote para transporte seco',
+        image: '0',
+        info: 'Equipe de apoio',
+        infoTwo: 'Segundo Motorista',
+        infoThree: 'Apenas R$9,99/Km',
+        infoFour: '',
+        price: '1599,90',
+    },
+    {
+        title: 'Pacote para transporte seco',
+        image: '0',
+        info: 'Equipe de apoio',
+        infoTwo: 'Segundo Motorista',
+        infoThree: 'Apenas R$9,99/Km',
+        infoFour: '',
+        price: '1599,90',
+    }
+]
+
 
 export default function ContentSmall() {
     return (
@@ -43,6 +74,30 @@ export default function ContentSmall() {
 
             <article className={styles.banner}>
                 <Banner />
+            </article>
+        </section>
+    )
+}
+
+export function ContentPrice() {
+    return (
+        <section className={styles.container}>
+            <article className={styles.prices}>
+                {PriceInfo.map((index, key) => {
+                    return (
+                        <CardPrice
+                            key={key}
+                            title={index.title}
+                            image={index.image}
+                            info={index.info}
+                            infoTwo={index.infoTwo}
+                            infoThree={index.infoThree}
+                            infoFour={index.infoFour}
+                            price={index.price}
+                        />
+                    )
+                })}
+
             </article>
         </section>
     )
