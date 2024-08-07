94ff1729c2a5b6287f7e877a0895e591c05f86f2
Paulo Henrique <paulo.bezerra2057@gmail.com>
2024-08-07 17:38:42 -0300

    fix: Home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,9 +2,10 @@ import { Header } from './../../components/Header';
 import { Main } from './../../components/Main';
 import { Footer } from './../../components/Footer';
 import ContentSmall, { ContentPrice } from '../../components/Cards';
-import Costumer from '../../components/Costumer';
 import Video from '../../components/Video';
 import Partner from '../../components/Partner';
+import { ContentCostumer } from '../../components/Costumer';
+import { client } from '../../data/carousel.json'
 
 
 export function Home() {
@@ -13,7 +14,7 @@ export function Home() {
             <Header />
             <Main titleWhite={'Sua'} titleYellow={'MUDANÇA'} tWhiteSecond={'é'} tWhiteThree={'nosso'} tYellowSecond={'Negócio!'} subtitle={'Desde 1991, estamos carregados de mudança e recomeço pelo Brasil. Conhecemos o peso da dedicação por força de vontade, siga conosco.'} />
             <ContentSmall />
-            <Costumer />
+            <ContentCostumer data={client} />
             <Video title={'Conheça nosso sonho da ChangeBee'} description={'Assista para saber mais quem somos!'} />
             <ContentPrice/>
             <Partner/>
