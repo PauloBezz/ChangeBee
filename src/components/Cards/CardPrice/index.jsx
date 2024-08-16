a5f402b72d65d7c53ed78ed5f46c7514015b5293
Paulo Henrique <paulo.bezerra2057@gmail.com>
2024-08-16 17:26:25 -0300

    fix: change structure items

--- src/components/Cards/CardPrice/index.jsx
+++ src/components/Cards/CardPrice/index.jsx
@@ -5,14 +5,20 @@ export function CardPrice({ title, image, info, infoTwo, infoThree, infoFour, pr
         <article className={styles.container}>
             <h1 className={styles.title}>{title}</h1>
             <div className={styles.image}>{image}</div>
-            <ul className={styles.list}>
-                <li>{info}</li>
-                <li>{infoTwo}</li>
-                <li>{infoThree}</li>
-                <li>{infoFour}</li>
-            </ul>
-            <h2 className={styles.price}>{price}</h2>
-            <button className={styles.btnContract}>Contratar</button>
+
+            <div className={styles.content}>
+                <div className={styles.list}>
+                    <p>{info}</p>
+                    <p>{infoTwo}</p>
+                    <p>{infoThree}</p>
+                    <p>{infoFour}</p>
+                </div>
+
+                <div>
+                    <h2 className={styles.price}>{price}</h2>
+                    <button className={styles.btnContract}>Contratar</button>
+                </div>
+            </div>
         </article>
     )
 }
