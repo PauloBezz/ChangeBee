47da042f0c0eb6cf3e97ffc209743eb0bb881fde
Paulo Henrique <paulo.bezerra2057@gmail.com>
2024-08-17 22:19:10 -0300

    fix: key error solution in Customer component

--- src/components/Costumer/index.jsx
+++ src/components/Costumer/index.jsx
@@ -3,40 +3,37 @@ import styles from './styles.module.css'
 import ArrowL from './assets/img/svg/ArrowLeft'
 import ArrowR from './assets/img/svg/ArrowRight'
 
-
 export const Costumer = ({ data }) => {
     const [slide, setSlide] = useState(0);
 
     const NextSlide = () => {
         setSlide(slide === data.length - 1 ? 0 : slide + 1);
     }
-    
+
     const PrevSlide = () => {
-        setSlide(slide ===  0 ? data.length -1 : slide - 1);
+        setSlide(slide === 0 ? data.length - 1 : slide - 1);
     }
 
     return (
         <section className={styles.carousel}>
-            {data.map((element, index) => {
-                console.log(index)
-                console.log(slide)
-
+            {data?.map((element, index) => {
+                const { title, description, image } = element;
                 return (
-                    <>
-                        <div key={index} className={slide === index ? styles.content : styles.contentNone}>
-                            <article className={styles.text}>
-                                <h1>{element.title}</h1>
-                                <p>{element.description}</p>
-                            </article>
-                            <article className={styles.image}>
-                                <img src={element.image} alt="Foto do cliente" />
-                            </article>
-                        </div>
-                        <div className={styles.buttons}>
-                            <button className={styles.button} key={index} onClick={PrevSlide}><ArrowL /></button>
+                    <div key={index} className={slide === index ? styles.content : styles.contentNone} >
+                        
+                        <article className={styles.text} >
+                            <h1 >{title}</h1>
+                            <p >{description}</p>
+                        </article>
+                        <article className={styles.image}>
+                            <img src={image} alt="Foto do cliente" />
+                        </article>
+
+                        <article className={styles.buttons} >
+                            <button className={styles.button} onClick={PrevSlide}><ArrowL /></button>
                             <button className={styles.button} onClick={NextSlide}><ArrowR /></button>
-                        </div>
-                    </>
+                        </article>
+                    </div>
                 );
             })}
         </section>
