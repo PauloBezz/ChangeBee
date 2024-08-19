dc79131c94996d520b5967f161f7d92484f8715e
Paulo Henrique <paulo.bezerra2057@gmail.com>
2024-08-18 23:58:15 -0300

    feat: add Sidebar

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,16 +3,28 @@ import logo from "./../../assets/logo.svg";
 import { Truck, TruckBlack } from "./../../assets/Button";
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import menu from '../../assets/menu.svg'
 
 
 export function Header() {
   const [hovered, setHovered] = useState(false);
+  const [visible, setVisible] = useState(false)
 
+  const handleOpen = () => {
+    setVisible(visible);
+  }
 
   return (
     <header className={styles.header}>
       <div className={styles.inline}>
         <Link className={styles.logo} to={""} ><img src={logo} alt="logo" /></Link>
+
+        <div>
+          <img src={menu} alt="" />
+        </div>
+        {visible && (
+          <Sidebar/>
+        )}
 
         <nav className={styles.nav}>
           <Link className={styles.link} to={""}>
@@ -31,7 +43,6 @@ export function Header() {
             Parceiros
           </Link>
         </nav>
-
 
         <Link className={styles.button} onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
           {hovered ? <TruckBlack /> : <Truck />}
