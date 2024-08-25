a410f402b23ab29b9a4361cca2d85e97c29d9f06
Paulo Henrique <paulo.bezerra2057@gmail.com>
2024-08-25 00:30:13 -0300

    refactor: add movie in Video component

--- src/components/Video/index.jsx
+++ src/components/Video/index.jsx
@@ -1,11 +1,12 @@
 import styles from './styles.module.css'
+import movie from './assets/movie.mp4'
 
 export default function Video({ title, description }) {
     return (
         <section id='video' className={styles.container}>
             <h1 className={styles.title}>{title}</h1>
             <div className={styles.video}>
-                <video src=""></video>
+                <video src={movie} controls></video>
             </div>
             <p className={styles.description}>{description}</p>
         </section>
