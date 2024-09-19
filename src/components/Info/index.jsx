06b74a084f237dee2c4fdd0af17fc0fc6a72baee
Paulo Henrique <paulo.bezerra2057@gmail.com>
2024-09-18 23:56:14 -0300

    refactor: change logic the map for current

--- src/components/Info/index.jsx
+++ src/components/Info/index.jsx
@@ -1,18 +1,14 @@
 import styles from './styles.module.css'
 
-export const Info = ({ data }) => {
+export const Info = ({ data, current }) => {
+    // será transferido para o componente Partner um item por vez
+    const { id, message, time, link, brandName } = data[current];
+
     return (
         <section className={styles.container}>
-            {data.map((info, index) => {
-                const { message, time, link, brandName } = info;
-                return (
-                    <>
-                        <h1 key={index} className={styles.message}>{message}</h1>
-                        <p className={styles.time}>{time}</p>
-                        <a href={link}>{brandName}</a>
-                    </>
-                )
-            })}
+            <h1 key={id} className={styles.message}>{message}</h1>
+            <p className={styles.time}>{time}</p>
+            <a href={link}>{brandName}</a>
         </section>
     )
 }
