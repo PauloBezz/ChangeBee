c065ec11d83e3652d057b51bb83183bc3cb506bf
Paulo Henrique <paulo.bezerra2057@gmail.com>
2024-09-20 02:07:11 -0300

    refactor: change logic

--- src/components/Partner/index.jsx
+++ src/components/Partner/index.jsx
@@ -1,5 +1,5 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
-import styles from './styles.module.css'
 import FourTruck from './assets/img/svg/Icon4Truck'
 import Good from './assets/img/svg/IconGood'
 import Graal from './assets/img/svg/IconGraal'
@@ -9,37 +9,45 @@ import Mercedes from './assets/img/svg/IconMercedes'
 import Stanley from './assets/img/svg/IconStanley'
 import { Info } from '../Info'
 import { brand } from '../../data/carousel.json'
-import { useState } from 'react'
-
+import styles from './styles.module.css'
 
 export default function Partner() {
+    const [visible, setVisible] = useState(false)
     const [info, setInfo] = useState(0)
 
-    const handleInfo = (index) =>{
+    const handleInfo = (index) => {
         setInfo(index)
+        if (index != info) {
+            setVisible(visible)
+        } else {
+            setVisible(!visible)
+        }
     }
 
     return (
         <section id='partner' className={styles.container}>
             <h1 className={styles.title}>Nossos parceiros de viagem</h1>
 
-            <article className={styles.icons}>
-                <Link onClick={() => handleInfo(0)}><FourTruck /></Link>
-                <Link onClick={() => handleInfo(1)}><Mercedes /></Link>
-                <Link onClick={() => handleInfo(2)}><Good /></Link>
+            <article className={styles.icons} >
+                <Link onClick={() => handleInfo(1)}><FourTruck /></Link>
+                <Link onClick={() => handleInfo(2)}><Mercedes /></Link>
+                <Link onClick={() => handleInfo(3)}><Good /></Link>
             </article>
 
-            <Info data={brand} current={info} />
+            {!visible && (
+                <Info data={brand} current={info} />
+            )
+            }
 
             <article className={styles.icons}>
-                <Link onClick={() => handleInfo(3)}><NuBank /></Link>
-                <Link onClick={() => handleInfo(4)}><Porto /></Link>
-                <Link onClick={() => handleInfo(5)}><Graal /></Link>
-                <Link onClick={() => handleInfo(6)}><Stanley /></Link>
+                <Link onClick={() => handleInfo(4)}><NuBank /></Link>
+                <Link onClick={() => handleInfo(5)}><Porto /></Link>
+                <Link onClick={() => handleInfo(6)}><Graal /></Link>
+                <Link onClick={() => handleInfo(7)}><Stanley /></Link>
             </article>
 
 
-            <p className={styles.description}>Somos gratos por todas parcerias das estradas dessa vida e a gratidão de nossos amados clientes é um presente. Vamos viajar juntos ao novo e com felicidade.</p>
+            <p className={styles.description}>Somos gratos por todas parcerias das estradas desta vida e a gratidão de nossos amados clientes é um presente. Vamos viajar juntos ao novo e com felicidade.</p>
         </section>
     )
 }
