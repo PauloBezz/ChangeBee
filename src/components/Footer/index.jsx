3353c60ff075b3fe7b9f0c300fdc8a407be84e04
Paulo Henrique <paulo.bezerra2057@gmail.com>
2024-09-20 02:03:50 -0300

    refactor: add Navigate

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,20 +1,36 @@
 import styles from './styles.module.css'
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { Whats } from "./assets/svg/Whatsapp"
 import { Face } from "./assets/svg/Facebook"
 import { Insta } from "./assets/svg/Instagram"
 
 export function Footer() {
+    const navigate = useNavigate();
+
+    const handleAlert = () => {
+        alert("Desculpe, estamos sem vagas no momento, mas em breve abriremos processos para estágio fique por dentro pelo o Linkedin")
+    }
+
+    const handleForScroll = (idLink) => {
+        navigate("/")
+        const subLink = document.getElementById(idLink)
+        if (subLink) {
+            subLink.scrollIntoView({ behavior: 'smooth' })
+        } else {
+            console.log("Algo deu errado")
+        }
+    }
+
     return (
         <footer className={styles.container}>
             <article className={styles.links}>
 
-                <a href="">Trabalhe conosco</a>
-                <a href="">Sobre nós</a>
+                <a onClick={() => handleAlert()}>Trabalhe conosco</a>
+                <a onClick={() => handleForScroll("video")}>Sobre nós</a>
                 <div className={styles.icons}>
-                    <Link target='_blank' to={'https://play.google.com/store/search?q=whatsapp&c=apps&hl=pt_BR'}><Whats /></Link>
-                    <Link target='_blank' to={'https://www.instagram.com/top_truck_caminhoes/'}><Insta /></Link>
-                    <Link target='_blank' to={'https://www.facebook.com/MercedesBenz/?locale=pt_BR'}><Face /></Link>
+                    <Link to={'https://play.google.com/store/search?q=whatsapp&c=apps&hl=pt_BR'}><Whats /></Link>
+                    <Link to={'https://www.instagram.com/top_truck_caminhoes/'}><Insta /></Link>
+                    <Link to={'https://www.facebook.com/MercedesBenz/?locale=pt_BR'}><Face /></Link>
                 </div>
             </article>
             <article className={styles.text}>
