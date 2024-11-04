3702296bd45306bc7b01e891d2d8c1a0192ed189
Paulo Henrique <paulo.bezerra2057@gmail.com>
2024-11-04 18:56:37 -0300

    feat: add descrition in Banner component

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -2,13 +2,13 @@ import styles from './styles.module.css'
 
 export const Banner = ({ data, current }) => {
 
-    const { figure } = data[current];
+    const { figure, description } = data[current];
 
     return (
         <article className={styles.container}>
             <h1>Antes do pé na estrada têm vantagens!</h1>
-            <img className={styles.figure} src={figure}  />
-            <p>Seria difícil não apresentar nossas principais vantagens para você, aqui na ChangeBee você não perde tempo, basta nos receber e notará um destaque sobre o fluxo de trabalho.</p>
+            <img className={styles.figure} src={figure} />
+            <p className={styles.text}>{description}</p>
         </article>
     )
 }
