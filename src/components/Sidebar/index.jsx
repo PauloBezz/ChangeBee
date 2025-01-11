67eac993fd1204954fd08fb465eeef70a07df740
PauloBezz <paulo.bezerra2057@gmail.com>
2025-01-10 23:58:18 -0300

    refactor: logic visible

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,17 +1,16 @@
-import styles from "./styles.module.css"
 import { Link, useNavigate } from "react-router-dom"
 import cancel from './assets/cancel.svg'
-import { useState } from "react";
+import styles from "./styles.module.css"
 
-export default function Sidebar({handleOpen}) {
+export default function Sidebar({ handleOpen }) {
   const navigate = useNavigate();
-  const [visible, setVisible] = useState(false)
 
   const handleForSection = (sectionId) => {
     navigate("/")
     const subLink = document.getElementById(sectionId);
     if (subLink) {
       subLink.scrollIntoView({ behavior: "smooth" });
+      handleOpen();
     }
   }
 
@@ -20,29 +19,28 @@ export default function Sidebar({handleOpen}) {
   }
 
   return (
-    <>
-      {!visible && (
-        <div className={styles.container}>
-          <nav className={styles.menu}>
-            <img src={cancel} alt="Close" className={styles.btnClone} onClick={handleOpen} />
-            <Link className={styles.link} onClick={() => handleForSection("advantage")}>
-              Vantagens
-            </Link>
-            <Link className={styles.link} onClick={() => handleForSection("client")}>
-              Clientes
-            </Link>
-            <Link className={styles.link} onClick={() => handleForSection("video")}>
-              Sonho
-            </Link>
-            <Link className={styles.link} onClick={() => handleForSection("price")}>
-              Preços
-            </Link>
-            <Link className={styles.link} onClick={() => handleForSection("partner")}>
-              Parceiros
-            </Link>
-          </nav>
-          <button className={styles.btnChange} onClick={handleChange}>Mudar</button>
-        </div>)}
-    </>
+    <main className={styles.container}>
+        <div className={styles.btnClose}>
+          <img src={cancel} alt="Close" onClick={handleOpen} />
+        </div>
+      <nav className={styles.menu}>
+        <Link className={styles.link} onClick={() => handleForSection("advantage")}>
+          Vantagens
+        </Link>
+        <Link className={styles.link} onClick={() => handleForSection("client")}>
+          Clientes
+        </Link>
+        <Link className={styles.link} onClick={() => handleForSection("video")}>
+          Sonho
+        </Link>
+        <Link className={styles.link} onClick={() => handleForSection("price")}>
+          Preços
+        </Link>
+        <Link className={styles.link} onClick={() => handleForSection("partner")}>
+          Parceiros
+        </Link>
+        <button className={styles.btnChange} onClick={handleChange}>Mudar</button>
+      </nav>
+    </main>
   )
 }
