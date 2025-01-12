f4863971a58513d3cc8960b61a0fe7d881fa9746
PauloBezz <paulo.bezerra2057@gmail.com>
2025-01-11 23:54:29 -0300

    feat: add Register component

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,6 +1,6 @@
-import { useState } from "react"
 import styles from "./styles.module.css"
-import { Link } from "react-router-dom"
+import { useState } from "react"
+import { Link, Navigate, useNavigate } from "react-router-dom"
 
 export function Login() {
 
@@ -14,12 +14,12 @@ export function Login() {
     }
 
     return (
-        <form className={styles.container}>
-            <section className={styles.content}>
+        <main className={styles.container}>
+            <form className={styles.content}>
                 <article className={styles.title}>
-                    <h1>Cadastro</h1>
+                    <h1>Login</h1>
                     <div className={styles.subtitle}>
-                        <h3>Não tem cadastro?</h3>
+                        <h3>Não tem conta?</h3>
                         <Link to={''}>Clique aqui</Link>
                     </div>
                 </article>
@@ -37,9 +37,58 @@ export function Login() {
                     </article>
                 </div>
                 <button className={styles.btnLogin} onClick={handleEmail}>
-                    Login
+                    Acessar
                 </button>
-            </section>
-        </form>
+            </form>
+        </main>
+    )
+}
+
+export function Register() {
+    const [user, setUser] = useState('')
+    const [email, setEmail] = useState('')
+    const [pass, setPass] = useState('')
+    const [confpass, setConfpass] = useState('')
+    const navigate = useNavigate()
+
+
+    const handleCreate = () => {
+        navigate('/register')
+    }
+
+    return (
+        <main>
+            <form>
+                <article className={styles.title}>
+                    <h1>Registro</h1>
+                </article>
+
+                <div className={styles.register}>
+                    <article>
+                        <h3>Nome completo</h3>
+                        <input type="text" required value={user} onChange={(e) => setUser(e.target.value)} />
+                    </article>
+
+                    <article>
+                        <h3>Email</h3>
+                        <input type="text" required value={email} onChange={(e) => setEmail(e.target.value)} />
+                    </article>
+
+                    <article>
+                        <h3>Senha</h3>
+                        <input type="password" required value={pass} onChange={(e) => setPass(e.target.value)} />
+                    </article>
+
+                    <article>
+                        <h3> Confirme a Senha</h3>
+                        <input type="password" required value={confpass} onChange={(e) => setConfpass(e.target.value)} />
+                        <Link>Já tenho conta</Link>
+                    </article>
+                </div>
+                <button className={styles.btnCreate} onClick={handleCreate}>
+                    Criar conta
+                </button>
+            </form>
+        </main>
     )
 }
