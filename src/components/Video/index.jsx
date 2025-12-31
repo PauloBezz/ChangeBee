47b8cbe7de4466c40323726ccbc908638e5ce90a
PauloBezz <paulo.bezerra2057@gmail.com>
2025-12-31 19:15:06 -0300

    fix: The call to action for the video has been corrected and is now an iframe with the corresponding link.

--- src/components/Video/index.jsx
+++ src/components/Video/index.jsx
@@ -1,14 +1,19 @@
-import styles from './styles.module.css'
-import movie from './assets/movie.mp4'
+import styles from "./styles.module.css";
 
 export default function Video({ title, description }) {
-    return (
-        <section id='video' className={styles.container}>
-            <h1 className={styles.title}>{title}</h1>
-            <div className={styles.video}>
-                <video src={movie} controls></video>
-            </div>
-            <p className={styles.description}>{description}</p>
-        </section>
-    )
+  return (
+    <section id="video" className={styles.container}>
+      <h1 className={styles.title}>{title}</h1>
+      <div className={styles.video}>
+        <iframe
+          src="https://www.youtube.com/embed/fVFK2DVViXk"
+          title="Como fazer preparo passo a passo"
+          loading="lazy"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        />
+      </div>
+      <p className={styles.description}>{description}</p>
+    </section>
+  );
 }
